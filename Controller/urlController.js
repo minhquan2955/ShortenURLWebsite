c91148458af576ac4a5ae4cd27ef9c4a3135bb75
minhquan2955 <minhquan2955@gmail.com>
2026-04-28 00:13:14 +0700

    feat: implement URL shortening controller with Redis integration

--- Controller/urlController.js
+++ Controller/urlController.js
@@ -1,16 +1,23 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
-const { Pool } = pg;
 import { PrismaClient } from "../generated/prisma/index.js";
 import crypto from "crypto";
 import QRCode from "qrcode";
+import redis from "redis";
 
+const { Pool } = pg;
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
-const generateCode = () => crypto.randomBytes(4).toString("base64url");
 
+const client = redis.createClient({
+  url: process.env.REDIS_URL || "redis://localhost:6379",
+});
+client.on("error", (err) => console.error(`Redis error: ${err}`));
+await client.connect();
+
+const generateCode = () => crypto.randomBytes(4).toString("base64url");
 const createShortUrl = async (req, res) => {
   try {
     const { originalURL, generateQR } = req.body;
@@ -34,17 +41,32 @@ const createShortUrl = async (req, res) => {
 
 const getOriginalUrl = async (req, res) => {
   const { shortCode } = req.params;
+  const cacheKey = `cache:url:${shortCode}`;
+  const cacheUrl = await client.get(cacheKey);
+  if (cacheUrl) {
+    // Url đã tồn tại trong cache
+    prisma.url
+      .update({
+        where: { shortCode },
+        data: { accessCounter: { increment: 1 } },
+      })
+      .catch((err) => console.error("Update counter error: ", err));
+    return res.redirect(301, cacheUrl);
+  }
+  //Url ko tồn tại trong cache
   const record = await prisma.url.findUnique({ where: { shortCode } });
   if (record) {
+    await client.setEx(cacheKey, 10800, record.originalURL);
     await prisma.url.update({
       where: { id: record.id },
       data: {
         accessCounter: { increment: 1 },
       },
     });
-    return res.status(302).redirect(record.originalURL);
+    return res.status(301).redirect(record.originalURL);
   }
+  //ko tồn tại trong cache và db
   res.status(404).send("URL not found");
 };
 
-export { createShortUrl, getOriginalUrl };
+export { createShortUrl, getOriginalUrl, client };
--- Router/route.js
+++ Router/route.js
@@ -1,18 +1,27 @@
 import express from "express";
-import { createShortUrl, getOriginalUrl } from "../Controller/urlController.js";
+import {
+  createShortUrl,
+  getOriginalUrl,
+  client,
+} from "../Controller/urlController.js";
 import { rateLimit } from "express-rate-limit";
-
+import RedisStore from "rate-limit-redis";
 const router = express.Router();
+
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   limit: 20, // Limit each IP to 20 requests per `window` (here, per 10 minutes).
   standardHeaders: "draft-8", // send `RateLimit` header let clients know they have how many requests
   legacyHeaders: false, // Disable the old headers.
   ipv6Subnet: 56, // Gom nhóm các IP cùng 1 subnet áp dụng limit
+  store: new RedisStore({
+    //lưu trữ số lần đã POST repuest của từng IP trên redis thay vì RAM
+    sendCommand: (...args) => client.sendCommand(args), //chọn redis client để gửi lệnh
+    prefix: "rl:shorten:", // tạo key với từng IP lưu vào redis: rl:shorten:IP
+  }),
   message: {
     error: "Too many short URLs created! Please try again later.",
   },
-  // store: ... , // Redis, Memcached, etc. See below.
 });
 //Create short URL/QrCode
 router.post("/shorten", limiter, createShortUrl); //Put rate limit for POST request
